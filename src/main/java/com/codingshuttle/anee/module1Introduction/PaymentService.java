c33134bc686b918c37b61733c12cf661a68ac206
anneDev <aneeLearning@2000>
2025-11-26 20:19:04 +0530

    Part-1, Beans Explained

---
+++ src/main/java/com/codingshuttle/anee/module1Introduction/PaymentService.java
@@ -0,0 +1,23 @@
+package com.codingshuttle.anee.module1Introduction;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+
+    public void pay() {
+        System.out.println("Paying...");
+    }
+
+    @PostConstruct
+    public void afterInit() {
+        System.out.println("Before Paying.");
+    }
+
+    @PreDestroy
+    public void beforeDestroy() {
+        System.out.println("After payment is done");
+    }
+}
